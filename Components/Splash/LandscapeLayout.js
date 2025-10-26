b043472515ec60383146ee891a7c9c7a171b9932
Musharaf Ahmed <musharafahmed780@gmail.com>
2025-10-26 23:55:19 +0500

    Splasg Both

---
+++ Components/Splash/LandscapeLayout.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import Heading from '../Heading';
+import Paragraph from '../Paragraph';
+import AuthButton from '../AuthButton';
+
+const LandscapeLayout = props => {
+  return (
+    <View style={{ flex: 1, flexDirection: 'row' }}>
+      <View
+        style={{
+          height: '100%',
+          width: '50%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Image
+          style={styles.image}
+          source={require('../../Assets/Images/splash.png')}
+        />
+      </View>
+
+      <View
+        style={{
+          height: '100%',
+          width: '50%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <View style={{ width: '90%' }}>
+          <Heading title="Welcome to The HR Application" />
+          <Paragraph
+            style={styles.paragraph}
+            text="Connect securely with potential candidates and employers through our HR application."
+          />
+          <AuthButton
+            onPress={props.onNavigate}
+            style={styles.button}
+            text="Continue"
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageContainer: {
+    height: hp('60%'),
+    width: wp('100%'),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    height: hp('35%'),
+    width: wp('100%'),
+    resizeMode: 'contain',
+  },
+  contentContainer: {
+    height: hp('40%'),
+    width: wp('100%'),
+  },
+  textWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: wp('100%'),
+  },
+  paragraph: {
+    marginVertical: hp('1%'),
+    textAlign: 'center',
+    paddingHorizontal: hp('2%'),
+  },
+  button: {
+    marginTop: hp('2%'),
+    width: '100%',
+  },
+});
+
+export default LandscapeLayout;
--- Screens/Auth/SplashScreen.js
+++ Screens/Auth/SplashScreen.js
@@ -1,75 +1,22 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
-import Heading from '../../Components/Heading';
-import Paragraph from '../../Components/Paragraph';
-import AuthButton from '../../Components/AuthButton';
-
+import { View, useWindowDimensions } from 'react-native';
+import PortraitLayout from '../../Components/Splash/PortraitLayout';
+import LandscapeLayout from '../../Components/Splash/LandscapeLayout';
 const SplashScreen = props => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require('../../Assets/Images/splash.png')}
-        />
-      </View>
+  const { width, height } = useWindowDimensions();
+  const isPortrait = width < height;
 
-      <View style={styles.contentContainer}>
-        <View style={styles.textWrapper}>
-          <Heading title="Welcome to The HR Application" />
-          <Paragraph
-            style={styles.paragraph}
-            text="Connect securely with potential candidates and employers through our HR application."
-          />
-          <AuthButton
-            onPress={() => {
-              props.navigation.replace('LoginScreen');
-            }}
-            style={styles.button}
-            text="Continue"
-          />
-        </View>
-      </View>
-    </View>
+  return isPortrait ? (
+    <PortraitLayout    onNavigate={() => {
+        props.navigation.replace('LoginScreen');
+      }} />
+  ) : (
+    <LandscapeLayout
+      onNavigate={() => {
+        props.navigation.replace('LoginScreen');
+      }}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  imageContainer: {
-    height: hp('60%'),
-    width: wp('100%'),
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    height: hp('35%'),
-    width: wp('100%'),
-    resizeMode: 'contain',
-  },
-  contentContainer: {
-    height: hp('40%'),
-    width: wp('100%'),
-  },
-  textWrapper: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: wp('100%'),
-  },
-  paragraph: {
-    marginVertical: hp('1%'),
-    textAlign: 'center',
-    paddingHorizontal: hp('2%'),
-  },
-  button: {
-    marginTop: hp('2%'),
-  },
-});
 
 export default SplashScreen;
