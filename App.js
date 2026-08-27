3978d97fef9323066fbdf06d56ace503b2d05433
Musharaf Ahmed <musharafahmed780@gmail.com>
2026-08-27 17:51:29 +0500

    Remove notifications

--- App.js
+++ App.js
@@ -17,9 +17,6 @@ import EmployeeDataReducer from './Store/Reducers/EmployeeDataReducer';
 import { toastConfig } from './utils/toastConfig';
 import DeviceInfo from 'react-native-device-info';
 import { SetIsTabletLanscape } from './Store/Actions/AuthAction';
-import messaging from '@react-native-firebase/messaging';
-import notifee, { AndroidImportance } from '@notifee/react-native';
-import { Alert, Platform } from 'react-native';
 
 // Store created outside component so it is never recreated on re-render
 const rootReducer = combineReducers({
@@ -42,66 +39,6 @@ const App = () => {
     };
     init();
   }, []);
-
-  const getFCMToken = async () => {
-    try {
-      const token = await messaging().getToken();
-      console.log('FCM Token:', token);
-    } catch (error) {
-      console.log('Error getting token:', error);
-    }
-  };
-
-  useEffect(() => {
-    if (Platform.OS === 'android') {
-      notifee.createChannel({
-        id: 'default',
-        name: 'Default Channel',
-        importance: AndroidImportance.HIGH,
-      });
-    }
-    getFCMToken();
-
-    // Subscribe and keep unsubscribe functions for cleanup
-    const unsubscribeOnMessage = messaging().onMessage(async remoteMessage => {
-      console.log('Foreground notification:', remoteMessage);
-      if (Platform.OS === 'android') {
-        await notifee.displayNotification({
-          title: remoteMessage.notification?.title || 'Notification',
-          body: remoteMessage.notification?.body || '',
-          android: {
-            channelId: 'default',
-            importance: AndroidImportance.HIGH,
-            smallIcon: 'ic_notification',
-          },
-        });
-      } else {
-        Alert.alert(
-          remoteMessage.notification?.title || 'Notification',
-          remoteMessage.notification?.body || '',
-        );
-      }
-    });
-
-    const unsubscribeOnNotificationOpened = messaging().onNotificationOpenedApp(remoteMessage => {
-      console.log('Background notification opened:', remoteMessage);
-    });
-
-    messaging()
-      .getInitialNotification()
-      .then(remoteMessage => {
-        if (remoteMessage) {
-          console.log('Quit state notification:', remoteMessage);
-        }
-      });
-
-    return () => {
-      unsubscribeOnMessage();
-      unsubscribeOnNotificationOpened();
-    };
-  }, []);
-
-
 
   return (
     <Provider store={store}>
--- Screens/DashboardScreen.js
+++ Screens/DashboardScreen.js
@@ -4,92 +4,10 @@ import { useSelector } from 'react-redux';
 import * as AuthAction from '../Store/Actions/AuthAction';
 import { View, Text } from 'react-native';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import ApiConstants from '../Constants/ApiConstants';
-import DeviceInfo from 'react-native-device-info';
-import messaging from '@react-native-firebase/messaging';
-import { Platform } from 'react-native'
 
 const SplashScreen = props => {
   const { isTablet, loginSuccess } = useSelector(state => state.auth);
   const dispatch = useDispatch();
-
-
-
-
-
-
-
-  const RegisterDevice = async () => {
-    const token = await messaging().getToken();
-    const PlatformName = Platform.OS === 'ios' ? 'iOS' : 'Android';
-    const VersionName = DeviceInfo?.getVersion().toString();
-    var myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append(
-      'Authorization',
-      `Bearer ${loginSuccess?.access_token}`,
-    );
-
-    var raw = JSON.stringify({
-      device_os: PlatformName,
-      device_id: token,
-      app_version: VersionName,
-    });
-
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow',
-    };
-
-    fetch(
-      `${ApiConstants.BaseUrl}/firebase/user/${loginSuccess?.user_id}/add_wf_device?db=${ApiConstants.DatabaseName}`,
-      requestOptions,
-    )
-      .then(response => response.json())
-      .then(result => {
-        console.log(JSON.stringify(result), "DEIVESSSSSS-===");
-      })
-      .catch(error => console.log('error', error));
-  };
-
-
-  const DeactivateAllDevices = async () => {
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append(
-      'Authorization',
-      `Bearer ${loginSuccess?.access_token}`,
-    );
-
-    var raw = JSON.stringify({
-      device_os: PlatformName,
-      device_id: token,
-      app_version: VersionName,
-    });
-
-    var requestOptions = {
-      method: 'PUT',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow',
-    };
-
-    fetch(
-      `${ApiConstants.BaseUrl}/firebase/user/${loginSuccess?.user_id}/deactivate_wf_extra_devices?db=${ApiConstants.DatabaseName}`,
-      requestOptions,
-    )
-      .then(response => response.json())
-      .then(result => {
-        console.log(JSON.stringify(result), "DeactivateAllDevices");
-      })
-      .catch(error => console.log('error', error));
-  };
-
-  useEffect(() => {
-    RegisterDevice()
-  }, [])
 
   return isTablet ? (
     <DashboardLandcape
